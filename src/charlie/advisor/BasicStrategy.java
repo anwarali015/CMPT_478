696eb3d168890988b4a1420dd6ed9a8ee4d89a5d
Raeya <Raeya@Anwar.home>
2014-02-23 16:45:28 -0500

    Updated File!

--- src/charlie/advisor/BasicStrategy.java
+++ src/charlie/advisor/BasicStrategy.java
@@ -6,65 +6,118 @@ import charlie.plugin.IAdvisor;
 import charlie.util.Play;
 import java.util.ArrayList;
 
+/**
+ *
+ * @author Mohammed Ali, Dan Blossom, Joseph Muro.
+ * Assignment: Charlie Advisor Plugin.
+ *
+ */
 public class BasicStrategy implements IAdvisor {
 
     private final int numbers[] = {17, 16, 15, 14, 13, 12, 11, 10, 9};
     ArrayList<ArrayList<Play>> numbers_list;
 
-    private final int numbers1[] = {8, 7, 6, 5};
-    ArrayList<ArrayList<Play>> numbers_list1;
-
-    private final int acs[] = {21, 20, 19, 18, 17, 16, 15, 14, 13};
-    ArrayList<ArrayList<Play>> acs_list;
+    private final int[] ace = {21, 20, 19, 18, 17, 16, 15, 14, 13};
+    ArrayList<ArrayList<Play>> ace_list;
 
     private final int pair[] = {16, 20, 18, 14, 12, 10, 8, 6, 4};
     ArrayList<ArrayList<Play>> pair_list;
 
+    /**
+     * Advise for the player
+     * 
+     * @param myHand the hand of the player
+     * @param upCard the dealer hand value
+     * @return advise according to the strategy card.
+     */
     @Override
     public Play advise(Hand myHand, Card upCard) {
         // throw new UnsupportedOperationException("Not supported yet.");
+        //Build and initilize the array.
         build();
-        int dhand = upCard.value();
+        // -2 on the dealer's upCard will give
+        // exact index value in the list.
+        int dhand = upCard.value() - 2;
         Play advise = null;
         if (myHand.isPair()) {
             //Go to the pair array 
-            int sum = getsum(myHand.getCard(0).value());
-            int index = getindex(pair, sum);
-            advise = pair_list.get(index).get(dhand - 2);
-        }else if (myHand.size()==2 && (myHand.getCard(0).getName().equals("A") || myHand.getCard(1).getName().equals("A"))){
+            int sum;
+            if (contains_ace(myHand)) {
+                //Possibility of Double Ace's
+                sum = get_sum(myHand);
+                if (sum == 12 || sum == 2) {
+                    //Two possible values of an ace pair
+                    advise = ace_list.get(0).get(dhand);
+                }
+                
+            } else {
+                //Not an ace so just perform the usual taskd
+                sum = get_sum(myHand);
+                int index = get_index(pair, sum);
+                advise = pair_list.get(index).get(dhand);
+            }
+
+        } else if (myHand.size() == 2 && contains_ace(myHand)) {
             //Go to the ac's array
-            int index = getindex(acs,myHand.getValue());
-            advise =  acs_list.get(index).get(dhand-2);
-        }else if (myHand.getValue() >= 9 && myHand.getValue() <= 17) {
+            int index = get_index(ace, myHand.getValue());
+            advise = ace_list.get(index).get(dhand);
+        } else if (myHand.getValue() >= 9 && myHand.getValue() <= 17) {
             //Go to number array 
-            int index = getindex(numbers, myHand.getValue());
-            advise = numbers_list.get(index).get(dhand - 2);
-        } else if (myHand.getValue() >= 5 && myHand.getValue() <= 8) { 
+            int index = get_index(numbers, myHand.getValue());
+            advise = numbers_list.get(index).get(dhand);
+        } else if (myHand.getValue() >= 5 && myHand.getValue() <= 8) {
             //Always hit between 5-8 inclusive
             advise = Play.HIT;
         } else {
             // Always Stay when 17+
             advise = Play.STAY;
-        } 
-        
+        }
+
         return advise;
     }
 
+    /**
+     * Builds and populates array.
+     */
     private void build() {
-        System.out.println("Number Array");
         build_numbers();
-        //build_numbers1();
-        System.out.println("Ac's Array");
         build_acs();
-        System.out.println("Pair Array");
         build_pair();
     }
 
-    private int getsum(int hand) {
-        return hand+hand;
+    /**
+     * Checks the hand for and ace.
+     *
+     * @param myhand the hand to check for an ace
+     * @return true if the hand contains an ace, false otherwise.
+     */
+    private boolean contains_ace(Hand myhand) {
+        boolean found = false;
+        for (int i = 0; i < myhand.size(); i++) {
+            if (myhand.getCard(0).isAce()) {
+                found = true;
+            }
+        }
+        return found;
     }
 
-    //Build pair list
+    /**
+     *Total value of the hand
+     * 
+     * @param hand to computer the sum
+     * @return the sum of the hand
+     */
+    private int get_sum(Hand hand) {
+        int total = 0;
+        for (int i = 0; i < hand.size(); i++) {
+            total += hand.getCard(i).value();
+        }
+        return total;
+    }
+
+    /**
+     * Builds and populates the pair array.
+     */
     private void build_pair() {
         pair_list = new ArrayList<>();
         for (int i = 0; i < pair.length; i++) {
@@ -98,68 +151,47 @@ public class BasicStrategy implements IAdvisor {
             populate(i, 1, 6, Play.SPLIT, pair_list);
             populate(i, 7, 10, Play.HIT, pair_list);
         }
-
-        for (int i = 0; i < pair_list.size(); i++) 
-            System.out.println(pair[i] + ": " + pair_list.get(i));
-        
     }
 
-    // build acs list
+    /**
+     * Builds the populates the ace's array.
+     */
     private void build_acs() {
-        acs_list = new ArrayList<>();
-        for (int i = 0; i < acs.length; i++) {
-            acs_list.add(new ArrayList<Play>());
+        ace_list = new ArrayList<>();
+        for (int i = 0; i < ace.length; i++) {
+            ace_list.add(new ArrayList<Play>());
         }
 
         //Row 1-3 (A10-A8)
         for (int i = 0; i < 3; i++) {
-            populate(i, 1, 10, Play.STAY, acs_list);
+            populate(i, 1, 10, Play.STAY, ace_list);
         }
         //Row 4 (A7)
-        populate(3, 1, 1, Play.STAY, acs_list);
-        populate(3, 2, 5, Play.DOUBLE_DOWN, acs_list);
-        populate(3, 6, 7, Play.STAY, acs_list);
-        populate(3, 8, 10, Play.HIT, acs_list);
+        populate(3, 1, 1, Play.STAY, ace_list);
+        populate(3, 2, 5, Play.DOUBLE_DOWN, ace_list);
+        populate(3, 6, 7, Play.STAY, ace_list);
+        populate(3, 8, 10, Play.HIT, ace_list);
         //Row 5 (A6)
-        populate(4, 1, 1, Play.HIT, acs_list);
-        populate(4, 2, 5, Play.DOUBLE_DOWN, acs_list);
-        populate(4, 6, 10, Play.HIT, acs_list);
+        populate(4, 1, 1, Play.HIT, ace_list);
+        populate(4, 2, 5, Play.DOUBLE_DOWN, ace_list);
+        populate(4, 6, 10, Play.HIT, ace_list);
         //Row 6-7 (A5) (A4)
         for (int i = 5; i <= 6; i++) {
-            populate(i, 1, 2, Play.HIT, acs_list);
-            populate(i, 3, 5, Play.DOUBLE_DOWN, acs_list);
-            populate(i, 6, 10, Play.HIT, acs_list);
+            populate(i, 1, 2, Play.HIT, ace_list);
+            populate(i, 3, 5, Play.DOUBLE_DOWN, ace_list);
+            populate(i, 6, 10, Play.HIT, ace_list);
         }
         //Row 8-9 (A3) (A2)
         for (int i = 7; i <= 8; i++) {
-            populate(i, 1, 3, Play.HIT, acs_list);
-            populate(i, 4, 5, Play.DOUBLE_DOWN, acs_list);
-            populate(i, 6, 10, Play.HIT, acs_list);
+            populate(i, 1, 3, Play.HIT, ace_list);
+            populate(i, 4, 5, Play.DOUBLE_DOWN, ace_list);
+            populate(i, 6, 10, Play.HIT, ace_list);
         }
-
-        for (int i = 0; i < acs_list.size(); i++) 
-            System.out.println(acs[i] + ": " + acs_list.get(i));
-        
     }
 
-    //build number1 list
-    private void build_numbers1() {
-        numbers_list1 = new ArrayList<>();
-        for (int i = 0; i < numbers1.length; i++) {
-            numbers_list1.add(new ArrayList<Play>());
-        }
-
-        //Row 1(8)
-        for (int i = 0; i < numbers1.length; i++) {
-            populate(i, 1, 10, Play.HIT, numbers_list1);
-        }
-
-        for (int i = 0; i < numbers_list1.size(); i++) 
-            System.out.println(numbers1[i] + ": " + numbers_list1.get(i));
-        
-    }
-
-    //build numbers list
+    /**
+     * Builds and populates the numbers array.
+     */
     private void build_numbers() {
         numbers_list = new ArrayList<>();
         for (int i = 0; i < numbers.length; i++) {
@@ -187,33 +219,40 @@ public class BasicStrategy implements IAdvisor {
         populate(8, 1, 1, Play.HIT, numbers_list);
         populate(8, 2, 5, Play.DOUBLE_DOWN, numbers_list);
         populate(8, 6, 10, Play.HIT, numbers_list);
-
-        for (int i = 0; i < numbers_list.size(); i++) 
-            System.out.println(numbers[i] + ": " + numbers_list.get(i));
-        
-
     }
 
-    //populate the given list
+    /**
+     * Populates the given array using the given advise.
+     *
+     * @param index the index of the array
+     * @param start the index to start
+     * @param end the index to end
+     * @param advise the advise to allocate
+     * @param list the arraylist to add the advise to.
+     */
     private void populate(int index, int start, int end, Play advise, ArrayList<ArrayList<Play>> list) {
         for (int i = start; i <= end; i++) {
             list.get(index).add(advise);
         }
+
     }
 
-    //get index of the target in the array.
-    private int getindex(int[] array, int target) {
+    /**
+     * Index of the specified value in the
+     * specified array
+     *
+     * @param array the array to find the index in.
+     * @param target the index of this target
+     * @return the index of the target
+     */
+    private int get_index(int[] array, int target) {
         int temp = -1;
         for (int i = 0; i < array.length; i++) {
             if (array[i] == target) {
                 temp = i;
             }
         }
+
         return temp;
-    }
-    
-    public static void main(String[] args) {
-       BasicStrategy b = new BasicStrategy();
-       b.build();
     }
 }
